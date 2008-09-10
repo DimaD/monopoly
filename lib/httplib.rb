1e1c6e1c61da9319363959d2842b844afc43344b
Dmitriy Dzema <dmitriy@dzema.name>
2008-09-10 12:15:55 +1100

    поправил отделение query_string, сделал выделение имени запрашиваемого фалйа

--- lib/httplib.rb
+++ lib/httplib.rb
@@ -2,15 +2,16 @@ require 'cgi'
 require 'socket'
 
 class HTTPRequest
-  attr_reader :method, :query_string, :version, :params, :port, :address
+  attr_reader :method, :query_string, :version, :params, :port, :address, :path, :file
   def initialize(str, s)
     @source = str
 
     lines = str.split("\n")
     first = lines.shift
     @method, @path, @version = first.split
-    @query_string = @path.sub(/^[^\?]+\?/,'')
-    
+    @query_string = @path.sub(/^\/[^\?]+\??/, '')
+    @file = @path.scan(/^\/?([^\?]+)\?/)
+
     @headers = {}
     lines.each do |l| 
       key, value = l.split(": ")
