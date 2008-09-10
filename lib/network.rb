730372dadca6f2099fd4bfa9879086d3b9e5f58e
Dmitriy Dzema <dmitriy@dzema.name>
2008-09-11 00:05:58 +1100

    Добавил список жоступных методов и их загрузку

--- lib/network.rb
+++ lib/network.rb
@@ -1,18 +1,30 @@
 require 'game_state'
 require 'exceptions'
 require 'httplib'
+require 'yaml'
 
 module Monopoly
 
   class Network
-
+    def initialize
+      @methods = YAML.load( File.new( File.dirname(__FILE__) + "/../conf/methods.yml" ) )
+    end
+    
     def process request
       puts ">>> connection from #{request.address}:#{request.port} => #{request.path} : #{request.file}"
-      return error404 unless 
+      return error404() unless allowed_method( request.file )
     end
 
     def ok
       "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nOK\n"
+    end
+    
+    def error404
+      "HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\nOK\n"
+    end
+    
+    def allowed_method m
+      @methods[m].nil?
     end
   end
 
--- lib/server.rb
+++ lib/server.rb
@@ -4,15 +4,23 @@ require 'evma_httpserver'
 require 'socket'
 require 'cgi'
 require 'httplib'
+require 'core'
+require 'network'
+
+def core
+  @core ||= Monopoly::Core.new(:save => 'default')
+end
+
+def monopoly
+  @monopoly ||= Monopoly::Network.new()
+end
 
 class MonopolyServer  < EventMachine::Connection
   def receive_data data
     s = get_sockname
     req = HTTPRequest.new(data, get_sockname)
     
-    puts ">>> connection from #{req.address}:#{req.port} => #{req.query_string}"
-    p req.params
-    send_data data
+    send_data monopoly.process(req) || "HTTP/1.0 500\n"
     close_connection_after_writing
   end
 end
